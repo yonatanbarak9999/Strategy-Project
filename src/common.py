cf96e244a5e7af8ef3a02d0f918990f4854f8221
yonatanbarak <yonatanbarak@campus.technion.ac.il>
2023-03-05 21:46:49 +0200

    Added script for finding all companies names.

---
+++ src/common.py
@@ -0,0 +1,20 @@
+"""
+Author: Yonatan Barak
+Date: 05/3/2023
+"""
+# ------ Imports ------ #
+
+import os
+
+# ------ Consts ------ #
+
+ASSETS_PATH = os.path.join('..', 'assets')
+
+
+# ------ Functions ------ #
+
+def create_assets_dir():
+    try:
+        os.mkdir(ASSETS_PATH)
+    except FileExistsError:
+        pass
